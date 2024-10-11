91fafc7321db188b37454bbf126bb2368224c385
Rui Gao <rgao0017@student.monash.edu>
2024-10-11 20:17:54 +1100

    Setup the project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@/assets/css/main.css';
 
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import App from '@/App.vue';
+import router from '@/router';
+
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+const app = createApp(App);
+app.use(pinia);
+app.use(router);
+app.mount('#app');
