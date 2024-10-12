fa15f604df6ed0b666e66ab6b2e61ce764e11088
Rui Gao <rgao0017@student.monash.edu>
2024-10-12 20:17:06 +1100

    Extract out GoogleMap.vue

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,87 @@
+<template>
+    <div ref="googleMapDom" style="height: 30rem;"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { Loader } from '@googlemaps/js-api-loader';
+
+const googleMapDom = ref(null);
+let googleMap = null;
+let infoWindow = null;
+let destMarker = null;
+
+const props = defineProps({
+    latLngBounds: {
+        type: Object,
+        default: {    // Melbourne location bounds for search restriction
+            north: -37.500,
+            south: -38.200,
+            west: 144.500,
+            east: 145.500,
+        },
+    },
+    center: { 
+        type: Object,
+        default: {    // Melbourne Center
+            lat: -37.8136,
+            lng: 144.9631,
+        }
+    },
+});
+
+const loader = new Loader({
+    apiKey: 'AIzaSyCSyP5_jP1x0ddEmPIHwC2MPZ18Rb84tv0', // the google map api key
+    libraries: ['places'],
+});
+
+onMounted(async () => {
+    await loader.load();
+
+    googleMap = new google.maps.Map(googleMapDom.value, {
+        center: props.center, // Melbourne Center
+        zoom: 8,
+        restriction: {
+            latLngBounds: props.latLngBounds,
+            strictBounds: true,
+        },
+    });
+    infoWindow = new google.maps.InfoWindow();
+});
+
+// Function to locate a place and set a marker
+const locate = (place) => {
+    if (!place?.geometry?.location) {
+        return alert('Please enter a valid location');
+    }
+
+    googleMap.panTo(place.geometry.location);
+
+    // Remove existing marker
+    destMarker?.setMap(null);
+
+    // Create a new marker
+    destMarker = new google.maps.Marker({
+        map: googleMap,
+        position: place.geometry.location,
+    });
+
+    destMarker.addListener('click', () => {
+        const contentString = `
+            <div>
+                <h5>${place.name}</h5>
+                <p class="text-muted">${place.vicinity}</p>
+                <img
+                    src="${place.photos[0].getUrl()}"
+                    alt="${place.name}"
+                    style="max-width: 15rem;"
+                />
+            </div>
+        `;
+        infoWindow.setContent(contentString);
+        infoWindow.open(googleMap, destMarker);
+    });
+};
+
+defineExpose({ locate });
+</script>
--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -17,91 +17,48 @@
             </div>
         </div>
         <div class="card-body">
-            <div ref="googleMapDom" style="height: 30rem;"></div>
+            <GoogleMap ref="mapRef" />
         </div>
     </div>
-
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
 import { Loader } from '@googlemaps/js-api-loader';
+import GoogleMap from '@/components/GoogleMap.vue';
 
 const searchInputDom = ref(null);
-const googleMapDom = ref(null);
-const googleMap = ref(null);
-const autocomplete = ref(null);
-
-let startMarker, destMarker;
+const mapRef = ref(null);
+let autocomplete = null;
 
 const loader = new Loader({
-    apiKey: 'AIzaSyCSyP5_jP1x0ddEmPIHwC2MPZ18Rb84tv0',    // the google map api key
+    apiKey: 'AIzaSyCSyP5_jP1x0ddEmPIHwC2MPZ18Rb84tv0', // the google map api key
     libraries: ['places'],
-})
+});
 
 // Melbourne location bounds for search restriction
 const melbourneBounds = {
     north: -37.500,
     south: -38.200,
     west: 144.500,
-    east: 145.500
+    east: 145.500,
 };
 
 onMounted(async () => {
     await loader.load();
-    const { Map } = await google.maps.importLibrary("maps");
-    const { Marker } = await google.maps.importLibrary("marker");
     const { Autocomplete } = await google.maps.importLibrary("places");
 
-    const infoWindow = new google.maps.InfoWindow(); // 创建信息窗口
-    googleMap.value = new Map(googleMapDom.value, {
-        center: { lat: -37.8136, lng: 144.9631 }, // Melbourne Center
-        zoom: 8,
-        restriction: {
-            latLngBounds: melbourneBounds,
-            strictBounds: true,
-        },
-    });
-
     // Autocomplete setup
-    autocomplete.value = new Autocomplete(searchInputDom.value, {
+    autocomplete = new Autocomplete(searchInputDom.value, {
         bounds: melbourneBounds,
         strictBounds: true,
         types: ['restaurant'], // Restrict results to restaurants
     });
 
-    autocomplete.value.addListener('place_changed', () => {
-        const place = autocomplete.value.getPlace();
-        console.log(place);
-        if (place && place.geometry && place.geometry.location) {
-            googleMap.value.panTo(place.geometry.location);
-
-            destMarker?.setMap(null)
-            destMarker = new Marker({
-                map: googleMap.value,
-                position: place.geometry.location,
-            });
-
-            destMarker.addListener('click', () => {
-                const contentString = `
-                    <div>
-                        <h5>${place.name}</h5>
-                        <p class="text-muted">${place.vicinity}</p>
-                        <img
-                            src="${place.photos ? place.photos[0].getUrl() : 'default-image-url'}"
-                            alt="${place.name}"
-                            style="max-width: 15rem;"
-                        />
-                    </div>
-                `;
-                infoWindow.setContent(contentString);
-                infoWindow.open(googleMap.value, destMarker);
-            });
-
-            googleMap.value.setCenter(place.geometry.location);
-            googleMap.value.setZoom(12);
-        }
+    // Relocate when user selects a place
+    autocomplete.addListener('place_changed', () => {
+        const place = autocomplete.getPlace();
+        mapRef.value.locate(place);
     });
-
 });
 </script>
