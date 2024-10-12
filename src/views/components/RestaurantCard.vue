2364339ea773c2c31309efc60dc91849813877f1
Rui Gao <rgao0017@student.monash.edu>
2024-10-13 01:00:05 +1100

    Implement RestaurantCard.vue

---
+++ src/views/components/RestaurantCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="row ">
+        <div class="col-md-6 order-md-2 p-3 text-end">
+            <img :src="place.photos[0].getUrl()" class="img-fluid mw-100" style="max-height: 20rem"
+                alt="the photo of the place" />
+        </div>
+        <div class="col-md-6 order-md-1 p-3">
+            <h2 class="fw-bold">{{ place.name }}</h2>
+            <p class="text-muted fst-italic">Address: {{ place.vicinity }}</p>
+            <p class="text-muted">Now is {{ place.opening_hours.isOpen() ? 'Opened' : 'Closed' }}</p>
+            <p class="text-muted">Call: {{ place.international_phone_number }}</p>
+            <p class="text-muted">Website: {{ place.website }}</p>
+            <p class="text-muted">Average Rating: {{ avgRating || 'no rating' }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    place: {
+        type: Object,
+        required: true
+    },
+    avgRating: {
+        type: Number,
+        required: false
+    }
+})
+</script>
